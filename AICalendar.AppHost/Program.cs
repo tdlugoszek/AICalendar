131855e2228a979b397006096729e097bccc9523
Tomasz Długoszek <tom.dlugoszek@gmail.com>
2025-06-14 22:11:22 +0200

    Fix project references and net version

--- AICalendar.AppHost/Program.cs
+++ AICalendar.AppHost/Program.cs
@@ -1,3 +1,14 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<AICalendar_ApiService>("api-service")
+    .WithHttpEndpoint(port: 5001, name: "api");
+
+
+
+
+
+
 
 builder.Build().Run();
