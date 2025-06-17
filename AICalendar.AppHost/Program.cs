8a38c3033b0abe37cf6e7ebb19694a9accf2fbd2
Tomasz Długoszek <tom.dlugoszek@gmail.com>
2025-06-18 01:42:12 +0200

    Configure Aspire AppHost

--- AICalendar.AppHost/Program.cs
+++ AICalendar.AppHost/Program.cs
@@ -1,14 +1,17 @@
-using Projects;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<AICalendar_ApiService>("api-service")
-    .WithHttpEndpoint(port: 5001, name: "api");
+//var sql = builder.AddSqlServer("sql", "sa", "YourPassword123!")
+//    .AddDatabase("AICalendar");
 
 
+var apiService = builder.AddProject<Projects.AICalendar_ApiService>("apiservice");
+  // .WithReference(sql);
 
 
+var mcpServer = builder.AddProject<Projects.AICalendar_MCPServer>("mcp-server");
 
 
+builder.AddProject<Projects.AICalendar_Web>("web")
+    .WithReference(apiService);
 
 builder.Build().Run();
