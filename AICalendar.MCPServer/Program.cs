bc7a47f6262abfd583e60b89c19880d8729aea6d
Tomasz Długoszek <tom.dlugoszek@gmail.com>
2025-06-18 01:32:34 +0200

    Configure MCPServer

--- AICalendar.MCPServer/Program.cs
+++ AICalendar.MCPServer/Program.cs
@@ -1,41 +1,28 @@
+using AICalendar.ServiceDefaults;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddServiceDefaults();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowApiClient", configurePolicy: policy =>
+    {
+        policy.AllowAnyOrigin()
+              .WithOrigins("https://localhost:5002") // ApiService URL
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddMcpServer()
+    .WithHttpTransport()
+    .WithToolsFromAssembly();
+
+
 
 var app = builder.Build();
+app.MapDefaultEndpoints();
+app.UseCors("AllowApiClient");
+app.MapMcp();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+await app.RunAsync();
